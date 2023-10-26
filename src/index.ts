3e6a1f9b18eb8f1588760d30740a3399b3f16930
kevin.allegretti1@marist.edu <Kevinallegretti17>
2023-10-26 19:38:17 -0400

    index stuff

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-import express from 'express';
+/*import express from 'express';
 import userRoutes from './routes/userRoutes';
 import path from 'path';
 import picksRoutes from './routes/picksRoutes'; // Update the path if needed
 import bodyParser from 'body-parser';
+import { Request, Response, NextFunction } from 'express';
+
+
 
 
 
@@ -14,6 +17,11 @@ const PORT = 3000;
 app.use(express.json());
 
 
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Welcome to Pick 6!');
@@ -42,39 +50,58 @@ app.get('/dashboard', (req, res) => {
 
 
 app.use(picksRoutes);
-// ... previous Express setup and middleware ...
-
-/*
-// Add a new pick
-app.post('/addPick', async (req, res) => {
-  const pickData = req.body;
-  try {
-    await addPick(pickData);
-    res.send('Pick added!');
-  } catch (error) {
-    res.status(500).send('Error adding pick');
-  }
-});
-
-// Get picks for a specific user
-app.get('/getUserPicks', async (req, res) => {
-  const username = req.query.username;
-  try {
-    const picks = await getPicksByUsername(username);
-    res.json(picks);
-  } catch (error) {
-    res.status(500).send('Error fetching picks');
-  }
-});
-*/
-
-// ... Start your Express server ...
 
 
 
 
 
 // Serve static files last
-app.use(express.static('public'));
+//app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, '../public')));
 
+*/
+
+import express, { Request, Response, NextFunction } from 'express';
+import userRoutes from './routes/userRoutes';
+import path from 'path';
+import picksRoutes from './routes/picksRoutes'; 
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = 3000;
+
+// 1. Middleware to parse JSON
+app.use(express.json());
+
+// 2. Logging middleware
+app.use((req: Request, res: Response, next: NextFunction) => {
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
+
+// 3. Body parser
+app.use(bodyParser.json());
+
+// 4. URL encoded parser
+app.use(express.urlencoded({ extended: true }));
+
+// 5. API routes
+app.get('/', (req, res) => {
+  res.send('Welcome to Pick 6!');
+});
+
+app.use('/users', userRoutes);
+
+app.get('/dashboard', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/dashboard.html'));
+});
+
+app.use(picksRoutes);
+
+// 6. Serve static files
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
