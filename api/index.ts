9bbd9c5bf762ee6b0fae314438827a913bf83884
kevin.allegretti1@marist.edu <Kevinallegretti17>
2023-10-31 19:24:42 -0400

    path changes 2.0

--- api/index.ts
+++ api/index.ts
@@ -30,7 +30,7 @@ app.use(express.urlencoded({ extended: true }));
   res.send('Welcome to Pick 6!');
 }); */
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/login.html'));
+  res.sendFile(path.join(__dirname, '../public/login.html'));
 
 });
 
