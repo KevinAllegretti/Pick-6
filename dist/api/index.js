c93d38712b5bcd460248feb6bd785969e5bd58ad
kevin.allegretti1@marist.edu <Kevinallegretti17>
2023-10-31 19:11:49 -0400

    new index

--- dist/api/index.js
+++ dist/api/index.js
@@ -1,66 +1,4 @@
 "use strict";
-/*import express from 'express';
-import userRoutes from './routes/userRoutes';
-import path from 'path';
-import picksRoutes from './routes/picksRoutes'; // Update the path if needed
-import bodyParser from 'body-parser';
-import { Request, Response, NextFunction } from 'express';
-
-
-
-
-
-const app = express();
-const PORT = 3000;
-
-
-// Use middleware to parse JSON
-app.use(express.json());
-
-
-
-app.use((req: Request, res: Response, next: NextFunction) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
-
-app.get('/', (req, res) => {
-  res.send('Welcome to Pick 6!');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
-app.use(bodyParser.json());
-app.use((req, res, next) => {
-  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  next();
-});
-
-
-app.use(express.urlencoded({ extended: true }));
-
-// Place all your API routes above the static file middleware
-app.use('/users', userRoutes);
-
-
-app.get('/dashboard', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/dashboard.html'));
-});
-
-
-app.use(picksRoutes);
-
-
-
-
-
-// Serve static files last
-//app.use(express.static('public'));
-app.use(express.static(path.join(__dirname, '../public')));
-
-*/
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -84,8 +22,11 @@ app.use(body_parser_1.default.json());
 // 4. URL encoded parser
 app.use(express_1.default.urlencoded({ extended: true }));
 // 5. API routes
+/*app.get('/', (req, res) => {
+  res.send('Welcome to Pick 6!');
+}); */
 app.get('/', (req, res) => {
-    res.send('Welcome to Pick 6!');
+    res.sendFile(path_1.default.join(__dirname, '../public/login.html'));
 });
 app.use('/users', userRoutes_1.default);
 app.get('/dashboard', (req, res) => {
