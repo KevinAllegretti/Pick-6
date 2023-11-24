6a1f7f097104d2072beac4446ca319c144038641
kevin.allegretti1@marist.edu <Kevinallegretti17>
2023-11-24 07:00:49 -0500

    deadline change

--- public/dashboard.js
+++ public/dashboard.js
@@ -442,7 +442,7 @@ function resetPicks() {
  
  
   function submitUserPicks() {
-    if (now < thursdayDeadline && now < tuesdayEndTime){
+    if (now < thursdayDeadline && now > tuesdayEndTime){
     isDeadline = false;
     }
   if (isDeadline == true){
